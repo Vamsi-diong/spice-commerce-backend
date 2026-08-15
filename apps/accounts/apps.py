c1b42ef4122527621de6b98be08c800e4949a706
Vamsi-doing <donkanavamsi223@gmail.com>
2026-08-15 13:59:49 +0530

    chore: initialize e-commerce backend

---
+++ apps/accounts/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+
+from django.apps import AppConfig
+
+
+class AccountsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.accounts"
+    label = "accounts"
